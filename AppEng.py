d9a022e10276740e787e707cf71c4c0d25f53a5b
Dylan Shen <shenpengdylan@gmail.com>
2020-03-28 13:34:51 +1300

    update

---
+++ AppEng.py
@@ -0,0 +1,93 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+#print(dash_core_components.__version__)
+
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
+app.layout = html.Div(children=[
+    html.H1(children='COVID-19 Information Integration'),
+
+    html.Div(children='''
+        Author: Dylan Shen 
+    '''),
+
+    html.Div(children='''
+        Github: https://github.com/Dylansppy/2019-nCov 
+    '''),
+
+    html.Div(children='''
+        LinkedIn: https://www.linkedin.com/in/dylan-shen-peng
+    '''),
+
+    dcc.Tabs(id="tabs", value='tab-1', children=[
+        dcc.Tab(label='Pandemic Situation', value='tab-1'),
+        dcc.Tab(label='Ministry of Health', value='tab-2'),
+        dcc.Tab(label='Other Resources', value='tab-3')
+    ]),
+
+    html.Div(id='tabs-content')
+
+    #dcc.Graph(
+        #id='example-graph',
+        #figure={
+            #'data': [
+                #{'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                #{'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            #],
+            #'layout': {
+                #'title': 'Dash Data Visualization'
+            #}
+        #}
+    #)
+])
+
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs', 'value')])
+
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+            html.Iframe(children='JHU',
+                        src="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6",
+                        height=500,
+                        width=1200)
+        ])
+
+    elif tab == 'tab-2':
+        return html.Div([
+            html.Div(children='''Ministry of Health'''),
+            html.Iframe(children='Ministry of Health',
+                        src="https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-current-situation/covid-19-current-cases",
+                        height=500,
+                        width=1250)
+        ])
+
+    elif tab == 'tab-3':
+        return html.Div([
+            html.Div(children='''WHO'''),
+            html.Iframe(children='WHO',
+                        src="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports",
+                        height=500,
+                        width=1250),
+
+            html.Div(children='''CDC'''),
+            html.Iframe(children='CDC',
+                        src="https://www.cdc.gov/coronavirus/2019-ncov/index.html",
+                        height=500,
+                        width=1250)
+        ])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ Data-crawler.py
@@ -0,0 +1,190 @@
+from bs4 import BeautifulSoup
+from service.db import DB
+from service.nameMap import country_type_map, city_name_map, country_name_map, continent_name_map
+import re
+import json
+import time
+import logging
+import datetime
+import requests
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'
+}
+
+
+class Crawler:
+    def __init__(self):
+        self.session = requests.session()
+        self.session.headers.update(headers)
+        self.db = DB()
+        self.crawl_timestamp = int()
+
+    def run(self):
+        while True:
+            self.crawler()
+            time.sleep(60)
+
+    def crawler(self):
+        while True:
+            self.crawl_timestamp = int(datetime.datetime.timestamp(datetime.datetime.now()) * 1000)
+            try:
+                r = self.session.get(url='https://3g.dxy.cn/newh5/view/pneumonia')
+            except requests.exceptions.ChunkedEncodingError:
+                continue
+            soup = BeautifulSoup(r.content, 'lxml')
+
+            overall_information = re.search(r'\{("id".*?)\]\}', str(soup.find('script', attrs={'id': 'getStatisticsService'})))
+            province_information = re.search(r'\[(.*?)\]', str(soup.find('script', attrs={'id': 'getListByCountryTypeService1'})))
+            area_information = re.search(r'\[(.*)\]', str(soup.find('script', attrs={'id': 'getAreaStat'})))
+            abroad_information = re.search(r'\[(.*)\]', str(soup.find('script', attrs={'id': 'getListByCountryTypeService2'})))
+            news = re.search(r'\[(.*?)\]', str(soup.find('script', attrs={'id': 'getTimelineService'})))
+            rumors = re.search(r'\[(.*?)\]', str(soup.find('script', attrs={'id': 'getIndexRumorList'})))
+
+            if not overall_information or not province_information or not area_information or not news:
+                continue
+
+            self.overall_parser(overall_information=overall_information)
+            self.province_parser(province_information=province_information)
+            self.area_parser(area_information=area_information)
+            self.abroad_parser(abroad_information=abroad_information)
+            self.news_parser(news=news)
+            self.rumor_parser(rumors=rumors)
+
+            break
+
+        logger.info('Successfully crawled.')
+
+    def overall_parser(self, overall_information):
+        overall_information = json.loads(overall_information.group(0))
+        overall_information.pop('id')
+        overall_information.pop('createTime')
+        overall_information.pop('modifyTime')
+        overall_information.pop('imgUrl')
+        overall_information.pop('deleted')
+        overall_information['countRemark'] = overall_information['countRemark'].replace(' 疑似', '，疑似').replace(' 治愈', '，治愈').replace(' 死亡', '，死亡').replace(' ', '')
+
+        if not self.db.find_one(collection='DXYOverall', data=overall_information):
+            overall_information['updateTime'] = self.crawl_timestamp
+
+            self.db.insert(collection='DXYOverall', data=overall_information)
+
+    def province_parser(self, province_information):
+        provinces = json.loads(province_information.group(0))
+        for province in provinces:
+            province.pop('id')
+            province.pop('tags')
+            province.pop('sort')
+            province['comment'] = province['comment'].replace(' ', '')
+
+            if self.db.find_one(collection='DXYProvince', data=province):
+                continue
+
+            province['provinceEnglishName'] = city_name_map[province['provinceShortName']]['engName']
+            province['crawlTime'] = self.crawl_timestamp
+            province['country'] = country_type_map.get(province['countryType'])
+
+            self.db.insert(collection='DXYProvince', data=province)
+
+    def area_parser(self, area_information):
+        area_information = json.loads(area_information.group(0))
+        for area in area_information:
+            area['comment'] = area['comment'].replace(' ', '')
+
+            # Because the cities are given other attributes,
+            # this part should not be used when checking the identical document.
+            cities_backup = area.pop('cities')
+
+            if self.db.find_one(collection='DXYArea', data=area):
+                continue
+
+            # If this document is not in current database, insert this attribute back to the document.
+            area['cities'] = cities_backup
+
+            area['countryName'] = '中国'
+            area['countryEnglishName'] = 'China'
+            area['continentName'] = '亚洲'
+            area['continentEnglishName'] = 'Asia'
+            area['provinceEnglishName'] = city_name_map[area['provinceShortName']]['engName']
+
+            for city in area['cities']:
+                if city['cityName'] != '待明确地区':
+                    try:
+                        city['cityEnglishName'] = city_name_map[area['provinceShortName']]['cities'][city['cityName']]
+                    except KeyError:
+                        print(area['provinceShortName'], city['cityName'])
+                        pass
+                else:
+                    city['cityEnglishName'] = 'Area not defined'
+
+            area['updateTime'] = self.crawl_timestamp
+
+            self.db.insert(collection='DXYArea', data=area)
+
+    def abroad_parser(self, abroad_information):
+        countries = json.loads(abroad_information.group(0))
+        for country in countries:
+            country.pop('id')
+            country.pop('tags')
+            country.pop('countryType')
+            country.pop('provinceId')
+            country.pop('cityName')
+            country.pop('sort')
+            # The original provinceShortName are blank string
+            country.pop('provinceShortName')
+            # Rename the key continents to continentName
+            country['continentName'] = country.pop('continents')
+            # Ding Xiang Yuan have a large number of duplicates,
+            # values are all the same, but the modifyTime are different.
+            # I suppose the modifyTime is modification time for all documents, other than for only this document.
+            # So this field will be popped out.
+            country.pop('modifyTime')
+            # createTime is also different even if the values are same.
+            # Originally, the createTime represent the first diagnosis of the virus in this area,
+            # but it seems different for abroad information.
+            country.pop('createTime')
+
+            country['comment'] = country['comment'].replace(' ', '')
+
+            if self.db.find_one(collection='DXYArea', data=country):
+                continue
+
+            country['countryName'] = country.get('provinceName')
+            country['provinceShortName'] = country.get('provinceName')
+            country['continentEnglishName'] = continent_name_map.get(country['continentName'])
+            country['countryEnglishName'] = country_name_map.get(country['countryName'])
+            country['provinceEnglishName'] = country_name_map.get(country['countryName'])
+
+            country['updateTime'] = self.crawl_timestamp
+
+            self.db.insert(collection='DXYArea', data=country)
+
+    def news_parser(self, news):
+        news = json.loads(news.group(0))
+        for _news in news:
+            _news.pop('pubDateStr')
+            if self.db.find_one(collection='DXYNews', data=_news):
+                continue
+            _news['crawlTime'] = self.crawl_timestamp
+
+            self.db.insert(collection='DXYNews', data=_news)
+
+    def rumor_parser(self, rumors):
+        rumors = json.loads(rumors.group(0))
+        for rumor in rumors:
+            rumor.pop('score')
+            rumor['body'] = rumor['body'].replace(' ', '')
+            if self.db.find_one(collection='DXYRumors', data=rumor):
+                continue
+            rumor['crawlTime'] = self.crawl_timestamp
+
+            self.db.insert(collection='DXYRumors', data=rumor)
+
+
+if __name__ == '__main__':
+    crawler = Crawler()
+    crawler.run()
