e490ea621b63a22a160af588b236ce22209daa39
Dylan Shen <shenpengdylan@gmail.com>
2020-03-28 14:32:22 +1300

    initial COVID-19 Eng

--- AppCN.py
+++ AppCN.py
@@ -17,7 +17,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div(children=[
-    html.H1(children='COVID-19 Information Integration'),
+    html.H1(children='2019 Novel Coronavirus (2019-nCoV) Information Integration'),
 
     html.Div(children='''
         Author: Dylan Shen 
@@ -32,9 +32,9 @@ app.layout = html.Div(children=[
     '''),
 
     dcc.Tabs(id="tabs", value='tab-1', children=[
-        dcc.Tab(label='Pandemic Situation', value='tab-1'),
-        dcc.Tab(label='Ministry of Health', value='tab-2'),
-        dcc.Tab(label='Other Resources', value='tab-3')
+        dcc.Tab(label='疫情数据+同程查询', value='tab-1'),
+        dcc.Tab(label='WHO+CDC', value='tab-2'),
+        dcc.Tab(label='资源信息', value='tab-3')
     ]),
 
     html.Div(id='tabs-content')
@@ -62,19 +62,20 @@ def render_content(tab):
             html.Iframe(children='JHU',
                         src="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6",
                         height=500,
-                        width=1200)
+                        width=1200),
+
+            html.Iframe(children='DXY',
+                        src="https://2019.ncov.wtf/",
+                        height=550,
+                        width=600),
+
+            html.Iframe(children='同程查询',
+                        src="https://sa.sogou.com/new-weball/page/sgs/epidemic/yyxw?type_page=yangshi&from=singlemessage&isappinstalled=0",
+                        height=550,
+                        width=600)
         ])
 
     elif tab == 'tab-2':
-        return html.Div([
-            html.Div(children='''Ministry of Health'''),
-            html.Iframe(children='Ministry of Health',
-                        src="https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-current-situation/covid-19-current-cases",
-                        height=500,
-                        width=1250)
-        ])
-
-    elif tab == 'tab-3':
         return html.Div([
             html.Div(children='''WHO'''),
             html.Iframe(children='WHO',
@@ -85,6 +86,39 @@ def render_content(tab):
             html.Div(children='''CDC'''),
             html.Iframe(children='CDC',
                         src="https://www.cdc.gov/coronavirus/2019-ncov/index.html",
+                        height=500,
+                        width=1250)
+        ])
+
+    elif tab == 'tab-3':
+        return html.Div([
+            html.Div(children='''义诊信息'''),
+            html.Iframe(children='义诊信息',
+                        src="https://shimo.im/sheets/JgXjYCJJTRQxJ3GP/MODOC/",
+                        height=500,
+                        width=1250),
+
+            html.Div(children='''医院物资'''),
+            html.Iframe(children='医院物资',
+                        src="https://shimo.im/sheets/k399pHyt6HKvW6xR/MODOC/",
+                        height=500,
+                        width=1250),
+
+            html.Div(children='''捐赠渠道'''),
+            html.Iframe(children='捐赠渠道',
+                        src="https://shimo.im/sheets/W3gxW6cwkYTDY6DD/",
+                        height=500,
+                        width=1250),
+
+            html.Div(children='''物资生产'''),
+            html.Iframe(children='物资生产',
+                        src="https://shimo.im/sheets/pchvJ6ddyRHHdXtv/MODOC/",
+                        height=500,
+                        width=1250),
+
+            html.Div(children='''物流信息'''),
+            html.Iframe(children='物流信息',
+                        src="https://shimo.im/sheets/RTHXp3ghtKXY3GcC/MODOC/",
                         height=500,
                         width=1250)
         ])
